b980a12561ed9ddd737312bd5c98257ac9486ae9
rithi <ritimahesh29@gmail.com>
2021-12-14 15:17:18 +0530

    last commit

---
+++ popup/fetch.js
@@ -0,0 +1,10 @@
+function loadXMLDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", " http://localhost:4000/location", true);
+  xhttp.send();
+}
